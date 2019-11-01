f494632f93ded1270895cfa810daa1085f96c36e
Dijah Deen <dijah@iams-MacBook-Pro.local>
2019-11-01 13:17:31 -0400

    this is my first commit

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,61 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+function CreateCard(headline, img, name) {
+
+    const Card = document.createElement('div')
+    const HeadLine = document.createElement('div')
+    const Author = document.createElement('div')
+    const ImageDiv = document.createElement('div')
+    const Image = document.createElement('img')
+    const NaMe = document.createElement('span')
+
+    Card.classList.add('card')
+    HeadLine.classList.add('headline')
+    Author.classList.add('author')
+    ImageDiv.classList.add('img-container')
+
+    Card.appendChild(HeadLine)
+    Card.appendChild(Author)
+    Author.appendChild(ImageDiv)
+    ImageDiv.appendChild(Image)
+    Author.appendChild(NaMe)
+
+
+    HeadLine.textContent = headline
+    NaMe.textContent = name
+    Image.src = img
+
+    return Card
+}
+
+const CardsContainer = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+    console.log(response)
+
+    response.data.articles.bootstrap.forEach(item => {
+        const NewArticle = CreateCard(item.headline, item.authorPhoto, item.authorName)
+        CardsContainer.appendChild(NewArticle)
+    })
+
+    response.data.articles.javascript.forEach(item => {
+        const NewArticle = CreateCard(item.headline, item.authorPhoto, item.authorName)
+        CardsContainer.appendChild(NewArticle)
+    })
+    response.data.articles.technology.forEach(item => {
+        const NewArticle = CreateCard(item.headline, item.authorPhoto, item.authorName)
+        CardsContainer.appendChild(NewArticle)
+
+    })
+    response.data.articles.jquery.forEach(item => {
+        const NewArticle = CreateCard(item.headline, item.authorPhoto, item.authorName)
+        CardsContainer.appendChild(NewArticle)
+    })
+    response.data.articles.node.forEach(item => {
+        const NewArticle = CreateCard(item.headline, item.authorPhoto, item.authorName)
+        CardsContainer.appendChild(NewArticle)
+    })
+})
